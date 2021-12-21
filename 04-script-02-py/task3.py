8ac70a9b37e0c71a64743b7a4a8c96bd1157e45a
Dmitry Kuchukov <kudim@inbox.ru>
2021-12-21 15:57:08 +0300

    выполнены 4 задачи по ДЗ 4.2

---
+++ 04-script-02-py/task3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+    bash_command = ["cd ~" + path, "git status"]
+    result_os = os.popen(' && '.join(bash_command)).read()
+    for result in result_os.split('\n'):
+        if result.find('modified') != -1:
+            prepare_result = result.replace('\tmodified:   ', '')
+            print(path + prepare_result)
+else:
+    print('Введите путь к репозиторию через параметр')
